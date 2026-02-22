054a98db68ea88231d6e60f161446e7ed59519a0
JELIN MAGEE <jelin.magee0@gmail.com>
2026-02-21 22:32:17 -0700

    Restructure into React version to use state-driven class toggling with CSS animation and transitions

--- lightbulb-effect/app.js
+++
@@ -1,7 +0,0 @@
-const light = document.querySelector("#bulb");
-const canvas = document.querySelector("#canvas");
-
-light.addEventListener("click", function () {
-  light.classList.toggle("is-on");
-  canvas.classList.toggle("dark");
-});
