a9409627388ba6e20f93c519285920e02b2c384e
JELIN MAGEE <jelin.magee0@gmail.com>
2026-02-21 22:05:36 -0700

    Update canvas and lightbulb position

--- lightbulb-effect/app.js
+++ lightbulb-effect/app.js
@@ -1,5 +1,7 @@
 const light = document.querySelector("#bulb");
+const canvas = document.querySelector("#canvas");
 
 light.addEventListener("click", function () {
   light.classList.toggle("is-on");
+  canvas.classList.toggle("dark");
 });
