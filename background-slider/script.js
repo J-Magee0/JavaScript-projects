76fa5f28a230edb7996e27b2888fc2490ec9c868
JELIN MAGEE <jelin.magee0@gmail.com>
2026-03-24 21:42:43 -0600

    Background-slider

---
+++ background-slider/script.js
@@ -0,0 +1,31 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
+
+const setBackground = () => {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+};
+
+const setActiveSlide = () => {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[activeSlide].classList.add("active");
+};
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++;
+  if (activeSlide > slides.length - 1) activeSlide = 0;
+  setBackground();
+  setActiveSlide();
+});
+
+leftBtn.addEventListener("click", () => {
+  activeSlide--;
+  if (activeSlide < 0) activeSlide = slides.length - 1;
+  setBackground();
+  setActiveSlide();
+});
+
+setBackground();
